3bf272562d940b315572d9678438dde95690869c
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-01-27 12:44:46 +0700

    Add game over logic

--- app.js
+++ app.js
@@ -52,6 +52,51 @@ function updateScore(addPts){
     document.getElementById('score-number').innerText = score;
     document.getElementById('best-number').innerText = highscore;
 }
+function getAllRows() {
+    let r1List = document.querySelectorAll('.row1');
+    let r2List = document.querySelectorAll('.row2');
+    let r3List = document.querySelectorAll('.row3');
+    let r4List = document.querySelectorAll('.row4');
+    let rows = [r1List,r2List,r3List,r4List];
+    return rows;
+}
+function getAllCols(){
+    let c1List = document.querySelectorAll('.col1');
+    let c2List = document.querySelectorAll('.col2');
+    let c3List = document.querySelectorAll('.col3');
+    let c4List = document.querySelectorAll('.col4');
+    let cols = [c1List, c2List, c3List, c4List];
+    return cols;
+}
+function isGameOver(){
+    if (document.querySelectorAll('.empty').length > 0){
+        return false;
+    } else {
+        let rows = getAllRows();
+        let cols = getAllCols();
+        let found = false;
+        let i = 0;
+        while (!found && i < 4) {
+            let row = rows[i], col = cols[i];
+            let j = 0;
+            while ( !found && j < 3) {
+                if ( (isBlockValEqual(row[j],row[j+1])) || (isBlockValEqual(col[j],col[j+1])) ){
+                    found = true;
+                } else {
+                    j++;
+                }
+            }
+            if (!found) {
+                i++;
+            }
+        }
+        if (!found){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 // MOVING FUNCTIONS
 
 function isCellOnEdge(elCell, dir) {
@@ -123,69 +168,66 @@ function isArrowKey (key) {
 
 function move(){
     window.addEventListener('keydown', function(e){
-        key = e.which;
-        if (isArrowKey(key)){
-            let r1List = document.querySelectorAll('.row1');
-            let r2List = document.querySelectorAll('.row2');
-            let r3List = document.querySelectorAll('.row3');
-            let r4List = document.querySelectorAll('.row4');
-            let c1List = document.querySelectorAll('.col1');
-            let c2List = document.querySelectorAll('.col2');
-            let c3List = document.querySelectorAll('.col3');
-            let c4List = document.querySelectorAll('.col4');
-            rowList = [r1List,r2List,r3List,r4List];
-            colList = [c1List, c2List, c3List, c4List];
-            let blockMoved = {num: 0};
-            if (key == 39 || key == 40){
-                let currentList;
-                let dir;
-                    if (key == 39){
-                        currentList = rowList;
-                        dir = "right";
-                    } else if (key == 40) {
-                        currentList = colList;
-                        dir = "down";
-                    }
-                    for (let i = 0,erl; erl = currentList[i]; i++){
-                        for (let j = (erl.length - 1);j >= 0; j--){ //iterate backward
-                            if (!isCellEmpty(erl[j])){
-                                if (!isCellOnEdge(erl[j], dir)) {
-                                    let moveDistance = 0;
-                                    while((j+moveDistance+1 <= erl.length-1)  && isCellEmpty(erl[j + moveDistance + 1])){
-                                        moveDistance++;
+        let gameOver = isGameOver();
+        if (!gameOver){
+            key = e.which;
+            if (isArrowKey(key)){
+                rowList = getAllRows();
+                colList = getAllCols();
+                let blockMoved = {num: 0};
+                if (key == 39 || key == 40){
+                    let currentList;
+                    let dir;
+                        if (key == 39){
+                            currentList = rowList;
+                            dir = "right";
+                        } else if (key == 40) {
+                            currentList = colList;
+                            dir = "down";
+                        }
+                        for (let i = 0,erl; erl = currentList[i]; i++){
+                            for (let j = (erl.length - 1);j >= 0; j--){ //iterate backward
+                                if (!isCellEmpty(erl[j])){
+                                    if (!isCellOnEdge(erl[j], dir)) {
+                                        let moveDistance = 0;
+                                        while((j+moveDistance+1 <= erl.length-1)  && isCellEmpty(erl[j + moveDistance + 1])){
+                                            moveDistance++;
+                                        }
+                                        animateBlock(erl, j, moveDistance, dir, blockMoved);
                                     }
-                                    animateBlock(erl, j, moveDistance, dir, blockMoved);
+                                }
+                            }
+                        }
+                } else if (key == 37 || key == 38){
+                    let currentList;
+                    let dir;
+                        if (key == 37){
+                            currentList = rowList;
+                            dir = "left";
+                        } else if (key == 38) {
+                            currentList = colList;
+                            dir = "up";
+                        }
+                        for (let i = 0,erl; erl = currentList[i]; i++){
+                            for (let j = 0;j <= (erl.length - 1); j++){ //iterate forward
+                                if (!isCellEmpty(erl[j])){
+                                    if (!isCellOnEdge(erl[j], dir)) {
+                                        let moveDistance = 0;
+                                        while((j-moveDistance-1 >= 0)  && isCellEmpty(erl[j - moveDistance - 1])){
+                                            moveDistance++;
+                                        }
+                                        animateBlock(erl, j, moveDistance, dir, blockMoved);
+                                    }
                                 }
                             }
                         }
                     }
-            } else if (key == 37 || key == 38){
-                let currentList;
-                let dir;
-                    if (key == 37){
-                        currentList = rowList;
-                        dir = "left";
-                    } else if (key == 38) {
-                        currentList = colList;
-                        dir = "up";
-                    }
-                    for (let i = 0,erl; erl = currentList[i]; i++){
-                        for (let j = 0;j <= (erl.length - 1); j++){ //iterate forward
-                            if (!isCellEmpty(erl[j])){
-                                if (!isCellOnEdge(erl[j], dir)) {
-                                    let moveDistance = 0;
-                                    while((j-moveDistance-1 >= 0)  && isCellEmpty(erl[j - moveDistance - 1])){
-                                        moveDistance++;
-                                    }
-                                    animateBlock(erl, j, moveDistance, dir, blockMoved);
-                                }
-                            }
-                        }
-                    }
+                if (blockMoved.num > 0){
+                    addNewRandomBlock(1);
                 }
-            if (blockMoved.num > 0){
-                addNewRandomBlock(1);
             }
+        } else {
+            alert("Game over");
         }
     });
 }
@@ -200,10 +242,10 @@ function resetGame(){
         }
         score = 0;
         document.getElementById('score-number').innerText = score;
-        // setTimeout(addNewRandomBlock(2), 300);
-        addNewRandomBlock(2);
+        addNewRandomBlock(2); 
     })
 }
+
 
 
 resetGame();
